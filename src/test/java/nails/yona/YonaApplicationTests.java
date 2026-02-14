b942e31563e9da7c56236934ae6c86d9a2ca916f
ImMaxence <maxence.bonnici2002@gmail.com>
2026-02-14 13:18:16 +0100

    init spring boot project with spring initializer

---
+++ src/test/java/nails/yona/YonaApplicationTests.java
@@ -0,0 +1,13 @@
+package nails.yona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
