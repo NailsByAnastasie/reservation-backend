f3f8e4c66d373c6b428f1425e9e1bbe39beb27d7
ImMaxence <maxence.bonnici2002@gmail.com>
2026-03-17 18:52:07 +0100

    oublie endpoint client

--- src/main/java/nails/yona/controller/ClientController.java
+++ src/main/java/nails/yona/controller/ClientController.java
@@ -9,6 +9,8 @@ import nails.yona.dto.response.ClientResponse;
 import nails.yona.service.ClientService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -52,7 +54,9 @@ public class ClientController {
 
     @Operation(operationId = "getAllClients")
     @GetMapping
-    public Page<ClientResponse> getAllClients(Pageable pageable) {
+    public Page<ClientResponse> getAllClients(
+            @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
         return clientService.getAllClients(pageable);
     }
 }
